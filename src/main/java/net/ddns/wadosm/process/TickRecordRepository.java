7a75d3d3f369086ea0d47ac84991565581e9d91e
Michał Wadowski <wadosm@gmail.com>
2025-01-17 14:39:04 +0100

    Split record dumping into separate project

--- src/main/java/net/ddns/wadosm/process/TickRecordRepository.java
+++
@@ -1,7 +0,0 @@
-package net.ddns.wadosm.process;
-
-import net.ddns.wadosm.process.entity.TickRecordEntity;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface TickRecordRepository extends MongoRepository<TickRecordEntity, String> {
-}
